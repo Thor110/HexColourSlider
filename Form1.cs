3ed98b5d125df2b7dd6a33d7c934aa64fb8eeeb6
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2025-03-30 03:17:30 +0100

    Perfect Execution

--- Form1.cs
+++ Form1.cs
@@ -9,8 +9,7 @@ namespace HexColourSlider
         private const float range = 10000000.0f; //4294967295 //2147483647
         private const int TrackBarIndexOffset = 8;
         private const int NumericUpDownIndexOffset = 13;
-        private int[] RGB = new int[3]; // store values for RGB numericUpDowns 4, 5 & 6
-        private bool inProgress; // is a calculation in progress
+        private int[] RGB = new int[3]; // store values for RGB numericUpDowns 4, 5 & 6 and to update the colour in pictureBox3
         private TrackBar[] trackBars;
         private NumericUpDown[] numericUpDowns;
         private PictureBox[] pictureBoxes;
@@ -27,18 +26,14 @@ namespace HexColourSlider
         }
         private void calculate(object sender, EventArgs e)
         {
-            if (inProgress) return; // prevents the event from being called recursively
-            inProgress = true;
-
-            basic = (float)value / range; // making use of the event handlers calling each other
-            ratio = (int)(basic * 255); // specifically the numericUpDown Value Changed Event Handler which calls this method.
-
             if (sender is TrackBar trackBar)
             {
                 index = int.Parse(trackBar.Name.Substring(TrackBarIndexOffset));
                 value = trackBar.Value;
-                numericUpDowns[index].Value = value;
-                numericUpDowns[index + 3].Value = ratio; // duplicate code // add three to account for the rgb numericUpDown controls
+                basic = (float)value / range; // making use of the event handlers calling each other
+                ratio = (int)(basic * 255); // specifically the numericUpDown Value Changed Event Handler which calls this method.
+                numericUpDowns[index].Text = value.ToString();
+                numericUpDowns[index + 3].Text = ratio.ToString(); // duplicate code // add three to account for the rgb numericUpDown controls
             }
             else if (sender is NumericUpDown numericUpDown)
             {
@@ -46,27 +41,26 @@ namespace HexColourSlider
                 if (index > 2)
                 {
                     value = (int)(((float)numericUpDown.Value / 255.0f) * range);
+                    basic = (float)value / range; // making use of the event handlers calling each other
+                    ratio = (int)(basic * 255); // specifically the numericUpDown Value Changed Event Handler which calls this method.
                     index -= 3; // remove three from the index when the index is greater than two to account for the rgb numericUpDown controls
                     trackBars[index].Value = value;
-                    numericUpDowns[index].Value = value; // reason for duplicate code
+                    numericUpDowns[index].Text = value.ToString(); // reason for duplicate code
                 }
                 else
                 {
                     value = (int)numericUpDown.Value;
+                    basic = (float)value / range; // making use of the event handlers calling each other
+                    ratio = (int)(basic * 255); // specifically the numericUpDown Value Changed Event Handler which calls this method.
                     trackBars[index].Value = value;
-                    numericUpDowns[index + 3].Value = ratio; // duplicate code // add three to account for the rgb numericUpDown controls
+                    numericUpDowns[index + 3].Text = ratio.ToString(); // duplicate code // add three to account for the rgb numericUpDown controls
                 }
             }
             RGB[index] = ratio;
-
             pictureBoxes[index].BackColor = Color.FromArgb((index == 0) ? RGB[0] : 0, (index == 1) ? RGB[1] : 0, (index == 2) ? RGB[2] : 0);
             pictureBoxes[3].BackColor = Color.FromArgb(RGB[0], RGB[1], RGB[2]);
-
             floatboxes[index].Text = basic.ToString();
-
             hexboxes[index].Text = BitConverter.ToUInt32(BitConverter.GetBytes(basic), 0).ToString("X8").Insert(2, " ").Insert(5, " ").Insert(8, " ");
-
-            inProgress = false;
         }
     }
 }
