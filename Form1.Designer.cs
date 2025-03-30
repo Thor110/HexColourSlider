deb5c565fa5ad3702e253006acf3bb413dda44de
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2025-03-30 02:25:12 +0100

    less calculations

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,86 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
+            trackBar0 = new TrackBar();
             trackBar1 = new TrackBar();
             trackBar2 = new TrackBar();
-            trackBar3 = new TrackBar();
-            pictureBox1 = new PictureBox();
+            pictureBox3 = new PictureBox();
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
+            pictureBox0 = new PictureBox();
+            pictureBox1 = new PictureBox();
             pictureBox2 = new PictureBox();
-            pictureBox3 = new PictureBox();
-            pictureBox4 = new PictureBox();
             label4 = new Label();
+            textBox3 = new TextBox();
             textBox4 = new TextBox();
             textBox5 = new TextBox();
-            textBox6 = new TextBox();
             label5 = new Label();
+            numericUpDown0 = new NumericUpDown();
             numericUpDown1 = new NumericUpDown();
             numericUpDown2 = new NumericUpDown();
-            numericUpDown3 = new NumericUpDown();
             label6 = new Label();
+            textBox0 = new TextBox();
             textBox1 = new TextBox();
             textBox2 = new TextBox();
-            textBox3 = new TextBox();
             label7 = new Label();
             label8 = new Label();
+            numericUpDown3 = new NumericUpDown();
             numericUpDown4 = new NumericUpDown();
             numericUpDown5 = new NumericUpDown();
-            numericUpDown6 = new NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)trackBar0).BeginInit();
             ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)trackBar2).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)trackBar3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox0).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown0).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown3).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown4).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown5).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown6).BeginInit();
             SuspendLayout();
+            // 
+            // trackBar0
+            // 
+            trackBar0.LargeChange = 1;
+            trackBar0.Location = new Point(61, 70);
+            trackBar0.Maximum = 10000000;
+            trackBar0.Name = "trackBar0";
+            trackBar0.Size = new Size(401, 45);
+            trackBar0.TabIndex = 3;
+            trackBar0.Scroll += calculate;
             // 
             // trackBar1
             // 
             trackBar1.LargeChange = 1;
-            trackBar1.Location = new Point(61, 70);
+            trackBar1.Location = new Point(61, 155);
             trackBar1.Maximum = 10000000;
             trackBar1.Name = "trackBar1";
             trackBar1.Size = new Size(401, 45);
-            trackBar1.TabIndex = 3;
+            trackBar1.TabIndex = 4;
             trackBar1.Scroll += calculate;
             // 
             // trackBar2
             // 
             trackBar2.LargeChange = 1;
-            trackBar2.Location = new Point(61, 155);
+            trackBar2.Location = new Point(61, 242);
             trackBar2.Maximum = 10000000;
             trackBar2.Name = "trackBar2";
             trackBar2.Size = new Size(401, 45);
-            trackBar2.TabIndex = 4;
+            trackBar2.TabIndex = 5;
             trackBar2.Scroll += calculate;
             // 
-            // trackBar3
+            // pictureBox3
             // 
-            trackBar3.LargeChange = 1;
-            trackBar3.Location = new Point(61, 242);
-            trackBar3.Maximum = 10000000;
-            trackBar3.Name = "trackBar3";
-            trackBar3.Size = new Size(401, 45);
-            trackBar3.TabIndex = 5;
-            trackBar3.Scroll += calculate;
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Location = new Point(61, 293);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(401, 50);
-            pictureBox1.TabIndex = 6;
-            pictureBox1.TabStop = false;
-            pictureBox1.BackColor = Color.Black;
+            pictureBox3.BackColor = Color.Black;
+            pictureBox3.Location = new Point(61, 293);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(401, 50);
+            pictureBox3.TabIndex = 6;
+            pictureBox3.TabStop = false;
             // 
             // label1
             // 
@@ -136,32 +136,32 @@
             label3.TabIndex = 9;
             label3.Text = "Blue";
             // 
+            // pictureBox0
+            // 
+            pictureBox0.BackColor = Color.Black;
+            pictureBox0.Location = new Point(468, 41);
+            pictureBox0.Name = "pictureBox0";
+            pictureBox0.Size = new Size(50, 50);
+            pictureBox0.TabIndex = 10;
+            pictureBox0.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Black;
+            pictureBox1.Location = new Point(468, 126);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(50, 50);
+            pictureBox1.TabIndex = 11;
+            pictureBox1.TabStop = false;
+            // 
             // pictureBox2
             // 
-            pictureBox2.Location = new Point(468, 41);
+            pictureBox2.BackColor = Color.Black;
+            pictureBox2.Location = new Point(468, 213);
             pictureBox2.Name = "pictureBox2";
             pictureBox2.Size = new Size(50, 50);
-            pictureBox2.TabIndex = 10;
+            pictureBox2.TabIndex = 12;
             pictureBox2.TabStop = false;
-            pictureBox2.BackColor = Color.Black;
-            // 
-            // pictureBox3
-            // 
-            pictureBox3.Location = new Point(468, 126);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(50, 50);
-            pictureBox3.TabIndex = 11;
-            pictureBox3.TabStop = false;
-            pictureBox3.BackColor = Color.Black;
-            // 
-            // pictureBox4
-            // 
-            pictureBox4.Location = new Point(468, 213);
-            pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(50, 50);
-            pictureBox4.TabIndex = 12;
-            pictureBox4.TabStop = false;
-            pictureBox4.BackColor = Color.Black;
             // 
             // label4
             // 
@@ -172,32 +172,32 @@
             label4.TabIndex = 13;
             label4.Text = "Range";
             // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(138, 41);
+            textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
+            textBox3.Size = new Size(125, 23);
+            textBox3.TabIndex = 14;
+            textBox3.Text = "00 00 00 00";
+            // 
             // textBox4
             // 
-            textBox4.Location = new Point(138, 41);
+            textBox4.Location = new Point(138, 126);
             textBox4.Name = "textBox4";
             textBox4.ReadOnly = true;
             textBox4.Size = new Size(125, 23);
-            textBox4.TabIndex = 14;
+            textBox4.TabIndex = 15;
             textBox4.Text = "00 00 00 00";
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(138, 126);
+            textBox5.Location = new Point(138, 213);
             textBox5.Name = "textBox5";
             textBox5.ReadOnly = true;
             textBox5.Size = new Size(125, 23);
-            textBox5.TabIndex = 15;
+            textBox5.TabIndex = 16;
             textBox5.Text = "00 00 00 00";
-            // 
-            // textBox6
-            // 
-            textBox6.Location = new Point(138, 213);
-            textBox6.Name = "textBox6";
-            textBox6.ReadOnly = true;
-            textBox6.Size = new Size(125, 23);
-            textBox6.TabIndex = 16;
-            textBox6.Text = "00 00 00 00";
             // 
             // label5
             // 
@@ -208,32 +208,32 @@
             label5.TabIndex = 17;
             label5.Text = "HEX (float32)";
             // 
+            // numericUpDown0
+            // 
+            numericUpDown0.Location = new Point(61, 41);
+            numericUpDown0.Maximum = new decimal(new int[] { 10000000, 0, 0, 0 });
+            numericUpDown0.Name = "numericUpDown0";
+            numericUpDown0.Size = new Size(71, 23);
+            numericUpDown0.TabIndex = 18;
+            numericUpDown0.ValueChanged += calculate;
+            // 
             // numericUpDown1
             // 
-            numericUpDown1.Location = new Point(61, 41);
+            numericUpDown1.Location = new Point(61, 126);
             numericUpDown1.Maximum = new decimal(new int[] { 10000000, 0, 0, 0 });
             numericUpDown1.Name = "numericUpDown1";
             numericUpDown1.Size = new Size(71, 23);
-            numericUpDown1.TabIndex = 18;
+            numericUpDown1.TabIndex = 19;
             numericUpDown1.ValueChanged += calculate;
             // 
             // numericUpDown2
             // 
-            numericUpDown2.Location = new Point(61, 126);
+            numericUpDown2.Location = new Point(61, 213);
             numericUpDown2.Maximum = new decimal(new int[] { 10000000, 0, 0, 0 });
             numericUpDown2.Name = "numericUpDown2";
             numericUpDown2.Size = new Size(71, 23);
-            numericUpDown2.TabIndex = 19;
+            numericUpDown2.TabIndex = 20;
             numericUpDown2.ValueChanged += calculate;
-            // 
-            // numericUpDown3
-            // 
-            numericUpDown3.Location = new Point(61, 213);
-            numericUpDown3.Maximum = new decimal(new int[] { 10000000, 0, 0, 0 });
-            numericUpDown3.Name = "numericUpDown3";
-            numericUpDown3.Size = new Size(71, 23);
-            numericUpDown3.TabIndex = 20;
-            numericUpDown3.ValueChanged += calculate;
             // 
             // label6
             // 
@@ -244,32 +244,32 @@
             label6.TabIndex = 21;
             label6.Text = "Colour";
             // 
+            // textBox0
+            // 
+            textBox0.Location = new Point(269, 41);
+            textBox0.Name = "textBox0";
+            textBox0.ReadOnly = true;
+            textBox0.Size = new Size(80, 23);
+            textBox0.TabIndex = 22;
+            textBox0.Text = "0";
+            // 
             // textBox1
             // 
-            textBox1.Location = new Point(269, 41);
+            textBox1.Location = new Point(269, 126);
             textBox1.Name = "textBox1";
             textBox1.ReadOnly = true;
             textBox1.Size = new Size(80, 23);
-            textBox1.TabIndex = 22;
+            textBox1.TabIndex = 23;
             textBox1.Text = "0";
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(269, 126);
+            textBox2.Location = new Point(269, 213);
             textBox2.Name = "textBox2";
             textBox2.ReadOnly = true;
             textBox2.Size = new Size(80, 23);
-            textBox2.TabIndex = 23;
+            textBox2.TabIndex = 24;
             textBox2.Text = "0";
-            // 
-            // textBox3
-            // 
-            textBox3.Location = new Point(269, 213);
-            textBox3.Name = "textBox3";
-            textBox3.ReadOnly = true;
-            textBox3.Size = new Size(80, 23);
-            textBox3.TabIndex = 24;
-            textBox3.Text = "0";
             // 
             // label7
             // 
@@ -289,111 +289,111 @@
             label8.TabIndex = 26;
             label8.Text = "RGB";
             // 
+            // numericUpDown3
+            // 
+            numericUpDown3.Location = new Point(355, 41);
+            numericUpDown3.Maximum = new decimal(new int[] { 255, 0, 0, 0 });
+            numericUpDown3.Name = "numericUpDown3";
+            numericUpDown3.Size = new Size(62, 23);
+            numericUpDown3.TabIndex = 27;
+            numericUpDown3.ValueChanged += calculate;
+            // 
             // numericUpDown4
             // 
-            numericUpDown4.Location = new Point(355, 41);
+            numericUpDown4.Location = new Point(355, 126);
             numericUpDown4.Maximum = new decimal(new int[] { 255, 0, 0, 0 });
             numericUpDown4.Name = "numericUpDown4";
             numericUpDown4.Size = new Size(62, 23);
-            numericUpDown4.TabIndex = 27;
+            numericUpDown4.TabIndex = 28;
             numericUpDown4.ValueChanged += calculate;
             // 
             // numericUpDown5
             // 
-            numericUpDown5.Location = new Point(355, 126);
+            numericUpDown5.Location = new Point(355, 214);
             numericUpDown5.Maximum = new decimal(new int[] { 255, 0, 0, 0 });
             numericUpDown5.Name = "numericUpDown5";
             numericUpDown5.Size = new Size(62, 23);
-            numericUpDown5.TabIndex = 28;
+            numericUpDown5.TabIndex = 29;
             numericUpDown5.ValueChanged += calculate;
-            // 
-            // numericUpDown6
-            // 
-            numericUpDown6.Location = new Point(355, 214);
-            numericUpDown6.Maximum = new decimal(new int[] { 255, 0, 0, 0 });
-            numericUpDown6.Name = "numericUpDown6";
-            numericUpDown6.Size = new Size(62, 23);
-            numericUpDown6.TabIndex = 29;
-            numericUpDown6.ValueChanged += calculate;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(535, 357);
-            Controls.Add(numericUpDown6);
             Controls.Add(numericUpDown5);
             Controls.Add(numericUpDown4);
+            Controls.Add(numericUpDown3);
             Controls.Add(label8);
             Controls.Add(label7);
-            Controls.Add(textBox3);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
+            Controls.Add(textBox0);
             Controls.Add(label6);
-            Controls.Add(numericUpDown3);
             Controls.Add(numericUpDown2);
             Controls.Add(numericUpDown1);
+            Controls.Add(numericUpDown0);
             Controls.Add(label5);
-            Controls.Add(textBox6);
             Controls.Add(textBox5);
             Controls.Add(textBox4);
+            Controls.Add(textBox3);
             Controls.Add(label4);
-            Controls.Add(pictureBox4);
-            Controls.Add(pictureBox3);
             Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(pictureBox0);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
-            Controls.Add(pictureBox1);
-            Controls.Add(trackBar3);
+            Controls.Add(pictureBox3);
             Controls.Add(trackBar2);
             Controls.Add(trackBar1);
+            Controls.Add(trackBar0);
             Name = "Form1";
             Text = "Colour Code Previewer";
+            ((System.ComponentModel.ISupportInitialize)trackBar0).EndInit();
             ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
             ((System.ComponentModel.ISupportInitialize)trackBar2).EndInit();
-            ((System.ComponentModel.ISupportInitialize)trackBar3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox0).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown0).EndInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown3).EndInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown4).EndInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDown5).EndInit();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown6).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
+        private TrackBar trackBar0;
         private TrackBar trackBar1;
         private TrackBar trackBar2;
-        private TrackBar trackBar3;
-        private PictureBox pictureBox1;
+        private PictureBox pictureBox3;
         private Label label1;
         private Label label2;
         private Label label3;
+        private PictureBox pictureBox0;
+        private PictureBox pictureBox1;
         private PictureBox pictureBox2;
-        private PictureBox pictureBox3;
-        private PictureBox pictureBox4;
         private Label label4;
+        private TextBox textBox3;
         private TextBox textBox4;
         private TextBox textBox5;
-        private TextBox textBox6;
         private Label label5;
+        private NumericUpDown numericUpDown0;
         private NumericUpDown numericUpDown1;
         private NumericUpDown numericUpDown2;
-        private NumericUpDown numericUpDown3;
         private Label label6;
+        private TextBox textBox0;
         private TextBox textBox1;
         private TextBox textBox2;
-        private TextBox textBox3;
         private Label label7;
         private Label label8;
+        private NumericUpDown numericUpDown3;
         private NumericUpDown numericUpDown4;
         private NumericUpDown numericUpDown5;
-        private NumericUpDown numericUpDown6;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,8 @@ namespace HexColourSlider
     {
         private int value; // temporarily store the RGB values in the full range 1-10000000
         private int index; // index of the control
+        private int ratio; // ratio of value to range times 255
+        private float basic; // basic ratio of value to range
         private const float range = 10000000.0f; //4294967295 //2147483647
         private const int TrackBarIndexOffset = 8;
         private const int NumericUpDownIndexOffset = 13;
@@ -14,17 +16,16 @@ namespace HexColourSlider
         private PictureBox[] pictureBoxes;
         private TextBox[] floatboxes;
         private TextBox[] hexboxes;
-        private float component;
         private uint uintValue;
         private string hexString = String.Empty; // value represented as a hexadecimal string float 32 value
         public Form1()
         {
             InitializeComponent();
-            trackBars = new TrackBar[] { trackBar1, trackBar2, trackBar3 };
-            numericUpDowns = new NumericUpDown[] { numericUpDown1, numericUpDown2, numericUpDown3, numericUpDown4, numericUpDown5, numericUpDown6 };
-            pictureBoxes = new PictureBox[] { pictureBox2, pictureBox3, pictureBox4, pictureBox1 };
-            floatboxes = new TextBox[] { textBox1, textBox2, textBox3 };
-            hexboxes = new TextBox[] { textBox4, textBox5, textBox6 };
+            trackBars = new TrackBar[] { trackBar0, trackBar1, trackBar2 };
+            numericUpDowns = new NumericUpDown[] { numericUpDown0, numericUpDown1, numericUpDown2, numericUpDown3, numericUpDown4, numericUpDown5 };
+            pictureBoxes = new PictureBox[] { pictureBox0, pictureBox1, pictureBox2, pictureBox3 };
+            floatboxes = new TextBox[] { textBox0, textBox1, textBox2 };
+            hexboxes = new TextBox[] { textBox3, textBox4, textBox5 };
         }
         private void calculate(object sender, EventArgs e)
         {
@@ -33,36 +34,46 @@ namespace HexColourSlider
 
             if (sender is TrackBar trackBar)
             {
-                index = int.Parse(trackBar.Name.Substring(TrackBarIndexOffset)) - 1;
+                index = int.Parse(trackBar.Name.Substring(TrackBarIndexOffset));
                 value = trackBar.Value;
                 numericUpDowns[index].Value = value;
-                numericUpDowns[index + 3].Value = (int)((value / range) * 255); // duplicate code // add three to account for the rgb numericUpDown controls
+
+                basic = (float)value / range; // duplication of code
+                ratio = (int)(basic * 255); // duplication of code
+
+                numericUpDowns[index + 3].Value = ratio; // duplicate code // add three to account for the rgb numericUpDown controls
             }
             else if (sender is NumericUpDown numericUpDown)
             {
-                index = int.Parse(numericUpDown.Name.Substring(NumericUpDownIndexOffset)) - 1;
+                index = int.Parse(numericUpDown.Name.Substring(NumericUpDownIndexOffset));
                 if (index > 2)
                 {
                     value = (int)(((float)numericUpDown.Value / 255.0f) * range);
                     index -= 3; // remove three from the index when the index is greater than two to account for the rgb numericUpDown controls
                     trackBars[index].Value = value;
                     numericUpDowns[index].Value = value; // reason for duplicate code
+
+                    basic = (float)value / range; // duplication of code
+                    ratio = (int)(basic * 255); // duplication of code
                 }
                 else
                 {
                     value = (int)numericUpDown.Value;
                     trackBars[index].Value = value;
-                    numericUpDowns[index + 3].Value = (int)((value / range) * 255); // duplicate code // add three to account for the rgb numericUpDown controls
+
+                    basic = (float)value / range; // duplication of code
+                    ratio = (int)(basic * 255); // duplication of code
+
+                    numericUpDowns[index + 3].Value = ratio; // duplicate code // add three to account for the rgb numericUpDown controls
                 }
             }
-            RGB[index] = (int)((value / range) * 255);
+            RGB[index] = ratio;
 
             pictureBoxes[index].BackColor = Color.FromArgb((index == 0) ? RGB[0] : 0, (index == 1) ? RGB[1] : 0, (index == 2) ? RGB[2] : 0);
             pictureBoxes[3].BackColor = Color.FromArgb(RGB[0], RGB[1], RGB[2]);
 
-            component = (float)value / range;
-            floatboxes[index].Text = component.ToString();
-            uintValue = BitConverter.ToUInt32(BitConverter.GetBytes(component), 0);
+            floatboxes[index].Text = basic.ToString();
+            uintValue = BitConverter.ToUInt32(BitConverter.GetBytes(basic), 0);
             hexString = uintValue.ToString("X8");
             hexboxes[index].Text = hexString.Insert(2, " ").Insert(5, " ").Insert(8, " ");
 
